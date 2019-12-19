32ea2c38b226b10425c4a0a1b58c4b61dcbca190
Maurício <mauriciobw17@gmail.com>
2019-12-18 21:46:15 -0300

    Updates

---
+++ precache-manifest.187c065f6905656c011a1d64a77e2a41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64aaf7be3a63cf0fe6275ccbf1d136b8",
+    "url": "/hash-game-react/index.html"
+  },
+  {
+    "revision": "bb6423fb53db839beadc",
+    "url": "/hash-game-react/static/css/main.e57bed77.chunk.css"
+  },
+  {
+    "revision": "d55700e8715f3ad6e54e",
+    "url": "/hash-game-react/static/js/2.0e165422.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/hash-game-react/static/js/2.0e165422.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb6423fb53db839beadc",
+    "url": "/hash-game-react/static/js/main.8b84e8be.chunk.js"
+  },
+  {
+    "revision": "17dbc0adb0df476ad7d8",
+    "url": "/hash-game-react/static/js/runtime-main.1e711867.js"
+  }
+]);
